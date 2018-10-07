8caea145fde7f918a7408638dfa580af55fd18d4
Renato Ribeiro <r3nato.rp+github@gmail.com>
2018-10-07 00:46:47 -0300

    Adicionando produto a dados de teste

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert into PRODUCT values (1, 'Batedeira Incrível', 'Batedeira que permite que você bata não só qualquer tipo de alimento, mas também diversos tipos de metal, pedras, tecidos e partes de animais.', 2499.99);
 insert into PRODUCT values (2, 'Motor de embarcação', 'Motor de alta produtividade similar ao utilizado pelo famoso e bem sucedido navio Titanic.', 80000.21);
-insert into PRODUCT values (3, 'Telefone Nokia 5120', 'Telefone moderno indispensável como ferramenta de desmoronamento de construções', 49.90);
+insert into PRODUCT values (3, 'Telefone Nokia 5120', 'Telefone moderno indispensável como ferramenta para desmoronamento de construções', 49.90);
+insert into PRODUCT values (4, 'Excaliburguer', 'Hamburguer forjado utilizando a lendária espada Excalibur. Utiliza ingredientes ainda datados da idade média. Ainda não se sabe a consequência de sua ingestão.', 559.90);
